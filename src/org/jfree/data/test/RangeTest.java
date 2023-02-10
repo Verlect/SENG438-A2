7bbc25885562465db7437d75727e574219419c3c
InfiniteTachyon <t60.david@gmail.com>
2023-02-09 17:27:38 -0700

    Commit From Eclipse Java

---
+++ src/org/jfree/data/test/RangeTest.java
@@ -0,0 +1,82 @@
+package org.jfree.data.test;
+
+import static org.junit.Assert.*; import org.jfree.data.Range; import org.junit.*;
+
+public class RangeTest {
+    private Range exampleRange;
+    private Range secondRange;
+    private Range thirdRange;
+    private Range sameValuesRange;
+    
+    @BeforeClass public static void setUpBeforeClass() throws Exception {
+    }
+
+
+    @Before
+    public void setUp() throws Exception { 
+    	exampleRange = new Range(-1, 1);
+    	secondRange = new Range (5, 30);
+    	thirdRange = new Range(-30, -5);
+    	sameValuesRange = new Range(0,0);
+    }
+
+
+    @Test
+    public void centralValueShouldBeZero() {
+        assertEquals("The central value of -1 and 1 should be 0",
+        0, exampleRange.getCentralValue(), .000000001d);
+    }
+    
+    
+    //Testing getLowerBound()
+    @Test
+    public void getNegativeLowerBound() {
+    	assertEquals("Incorrect Lower Bound was returned.", -1, exampleRange.getLowerBound(), .000000001d);
+    }
+    
+    @Test
+    public void getPositiveLowerBound() {
+    	assertEquals("Incorrect Lower Bound was returned.", 5, secondRange.getLowerBound(), .000000001d);
+    }
+    
+    // Testing getUpperBound()
+    @Test
+    public void getNegativeUpperBound() {
+    	assertEquals("Incorrect Upper Bound was returned.", -5, thirdRange.getUpperBound(), .000000001d);
+    }
+    
+    @Test
+    public void getPositiveUpperBound() {
+    	assertEquals("Incorrect Upper Bound was returned.", 30, secondRange.getUpperBound(), .000000001d);
+    } 
+    
+    // Testing toString()
+    @Test
+    public void correctToStringWithPositiveUpperAndLower() {
+    	assertEquals("Incorrect String Returned.", "Range[5.0,30.0]", secondRange.toString());
+    }
+    
+    @Test
+    public void correctToStringWithNegativeUpperAndLower() {
+    	assertEquals("Incorrect String Returned.", "Range[-30.0,-5.0]", thirdRange.toString());
+    }
+    
+    @Test
+    public void correctToStringWithNegativeLowerAndPositiveUpper() {
+    	assertEquals("Incorrect String Returned.", "Range[-1.0,1.0]", exampleRange.toString());
+    }
+    
+    @Test
+    public void correctToStringWithEquivilentLowerAndUpper() {
+    	assertEquals("Incorrect String Returned.", "Range[0.0,0.0]", sameValuesRange.toString());
+    }
+    
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+}
